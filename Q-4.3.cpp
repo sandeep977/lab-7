36e59673321af4032021172b369302d7cb318bed
sandeep977 <31313335+sandeep977@users.noreply.github.com>
2017-11-11 00:13:34 +0530

    Create Q-4.3.cpp

---
+++ Q-4.3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// Declaring a point using default class constructor (x = y = 0):
+  Point p1;
+  cout<< "The x value for p1 is " << p1.Get_X() << endl;
+  cout<< "The y value for p1 is " << p1.Get_Y() << endl;
+
+// Declaring a point with coordinates X = 2, Y = 3:
+  Point p2(2, 3);
+  cout<< "The x value for p2 is " << p2.Get_X() << endl;
+  cout<< "The y value for p2 is " << p2.Get_Y() << endl;
+
+// Moving point p2 by (1, -1):
+  p2.Move(1, -1);
+  cout<< "After the move:" << endl;
+  cout<< "The x value for p2 is " << p2.Get_X() << endl;
+  cout<< "The y value for p2 is " << p2.Get_Y() << endl;
+
+//Lab 7 exercise 4.1. Test member function print on points p1, p2:
+
+//Lab 7 Exercises 4.2, 4.3. Testing of the class Rectangle goes here:  
+
+  return 0;
+}
